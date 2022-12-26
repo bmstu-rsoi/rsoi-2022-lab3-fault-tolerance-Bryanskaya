b7cf3dd4c1ec7fbd4858febcb2f22ac38a7ed453
Bryanskaya <bryanskayakatya@yandex.ru>
2022-12-26 17:36:19 +0300

    ADD: main logic

--- Gateway/src/main/java/ru/bmstu/gateway/dto/LoyaltyInfoResponse.java
+++ Gateway/src/main/java/ru/bmstu/gateway/dto/LoyaltyInfoResponse.java
@@ -1,13 +1,17 @@
 package ru.bmstu.gateway.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 import org.springframework.validation.annotation.Validated;
 import ru.bmstu.gateway.dto.enums.UserStatusEnum;
 
 @Data
 @Validated
+@NoArgsConstructor
 @Accessors(chain = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class LoyaltyInfoResponse {
     private UserStatusEnum status;
     private Integer discount;
