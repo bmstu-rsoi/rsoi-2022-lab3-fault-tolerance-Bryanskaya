c36a243f0153fe51ca525b868d26f0207c88df7e
Bryanskaya <bryanskayakatya@yandex.ru>
2022-12-26 19:04:17 +0300

    UPD: return to default values for timeout

--- scripts/wait-script.sh
+++ scripts/wait-script.sh
@@ -5,8 +5,6 @@ path=$(dirname "$0")
 
 PIDs=()
 for port in "${PORTS[@]}"; do
-  echo "******"
-  echo port
   "$path"/wait-for.sh -t 120 "http://localhost:$port/manage/health" -- echo "Host localhost:$port is active" &
   PIDs+=($!)
 done
